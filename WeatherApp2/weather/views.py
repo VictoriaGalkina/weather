9b704748700b00d9f3f1e9e2ec4560faf982d2ad
Pc <viktoriagalkina292@gmail.com>
2023-03-11 19:57:55 +0300

    weather app

---
+++ WeatherApp2/weather/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+import requests
+from .models import City
+from .forms import CityForm
+
+
+def index(request):
+    appid = "31d76a19a89f0ee643d7c16435e52571"
+    url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=" + appid
+
+
+    if(request.method == 'POST'):
+        form = CityForm(request.POST)
+        form.save()
+
+    form = CityForm
+
+    cities = City.objects.all()
+
+    all_cities = []
+
+    for city in cities:
+        res = requests.get(url.format(city.name)).json()
+        city_info = {
+           'city': city.name,
+           'temp': res["main"]["temp"],
+           'icon': res["weather"][0]["icon"]
+        }
+
+    all_cities.append(city_info)
+
+
+    context = {'all_info': all_cities, 'form': form}
+
+    return render(request, 'index.html', context)
